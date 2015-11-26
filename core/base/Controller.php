2bb2484d83636e7aa921a03e46326c4fbd9cd282
reydel90 <reydel25@icloud.com>
2015-11-26 18:28:01 +0100

    template test 2

--- core/base/Controller.php
+++ core/base/Controller.php
@@ -3,8 +3,8 @@ class Controller{
 	protected $template = 'default';
 			  
 	public function view($view, $data = []){
-		require_once TEMPLATES . $this->template . 'start.php';
+		require_once TEMPLATES . $this->template . DS . 'start.php';
 		require_once VIEWS . $view . '.php';
-		require_once TEMPLATES . $this->template . 'end.php';
+		require_once TEMPLATES . $this->template . DS . 'end.php';
 	}
 }
