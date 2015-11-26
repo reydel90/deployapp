3989726d7fe0117c183850b04d957e4d1d58f9e6
reydel90 <reydel25@icloud.com>
2015-11-26 19:15:50 +0100

    check if view exists test 5

--- core/base/Controller.php
+++ core/base/Controller.php
@@ -3,7 +3,6 @@ class Controller{
 	protected $template  = 'default',
 			  $starpage  = 'start.php',
 			  $endpage   = 'end.php',
-			  $view      = 'landing.php',
 			  $routeView = '';
 
 			  
@@ -13,12 +12,9 @@ class Controller{
 			unset($url[0]);
 		}
 		$this->routeView = TEMPLATES . $this->template . DS;
-		if(file_exists(VIEWS . $view . '.php')){
-			$this->view = $view . '.php';
-		}
-		
+
 		require_once $this->routeView . $this->starpage;
-		require_once VIEWS . $this->$view;
+		require_once VIEWS . $view . '.php';
 		require_once $this->routeView . $this->endpage;
 	}
 }
