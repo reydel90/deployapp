93f4f569f97a875bf6a5d196556d0a038495c930
reydel90 <reydel25@icloud.com>
2015-11-26 18:32:05 +0100

    test tesmplate 4 url

--- core/base/Controller.php
+++ core/base/Controller.php
@@ -3,6 +3,8 @@ class Controller{
 	protected $template = 'default';
 			  
 	public function view($view, $data = []){
+		$url = Url::parse();
+		print_r($url);
 		require_once TEMPLATES . $this->template . DS . 'start.php';
 		require_once VIEWS . $view . '.php';
 		require_once TEMPLATES . $this->template . DS . 'end.php';
