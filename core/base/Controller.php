d0b58bcdf04caf50b339a770cd52b73e68b22839
reydel90 <reydel25@icloud.com>
2015-11-26 18:35:17 +0100

    unset [0] if is empty

--- core/base/Controller.php
+++ core/base/Controller.php
@@ -4,6 +4,9 @@ class Controller{
 			  
 	public function view($view, $data = []){
 		$url = Url::parse();
+		if(empty($url[0])){
+			unset($url[0]);
+		}
 		print_r($url);
 		require_once TEMPLATES . $this->template . DS . 'start.php';
 		require_once VIEWS . $view . '.php';
